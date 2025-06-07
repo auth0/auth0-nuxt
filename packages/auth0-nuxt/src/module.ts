7c85046734c5a851981d6bf22a33394982a3f564
Frederik Prijck <frederik.prijck@okta.com>
2025-06-07 23:31:07 +0200

    feat: add basic plugin

---
+++ packages/auth0-nuxt/src/module.ts
@@ -0,0 +1,17 @@
+import {
+  defineNuxtModule,
+  createResolver,
+  addServerPlugin,
+} from '@nuxt/kit';
+
+export default defineNuxtModule({
+  meta: {
+    name: 'auth0-nuxt',
+    configKey: 'auth0',
+  },
+  async setup(_options, nuxt) {
+    const resolver = createResolver(import.meta.url);
+
+    addServerPlugin(resolver.resolve('./runtime/server/plugins/auth.server'));
+  },
+});
---
+++ packages/auth0-nuxt/src/runtime/server/plugins/auth.server.ts
@@ -0,0 +1,45 @@
+import { useRuntimeConfig } from '#imports';
+import { ServerClient } from '@auth0/auth0-server-js';
+import { defineNitroPlugin } from 'nitropack/dist/runtime/plugin';
+import { CookieTransactionStore } from '../../../store/cookie-transaction-store';
+import { StatelessStateStore } from '../../../store/stateless-state-store';
+
+declare module 'h3' {
+  interface H3EventContext {
+    auth0Client: ServerClient<{ event: H3Event }>;
+  }
+}
+
+export default defineNitroPlugin((nitroApp) => {
+  const config = useRuntimeConfig();
+  const options: any = config.auth0;
+
+  if (!options.domain) throw new Error('Auth0 configuration error: Domain is required');
+  if (!options.clientId) throw new Error('Auth0 configuration error: Client ID is required');
+  if (!options.clientSecret) throw new Error('Auth0 configuration error: Client Secret is required');
+  if (!options.appBaseUrl) throw new Error('Auth0 configuration error: App Base URL is required');
+  if (!options.sessionSecret) throw new Error('Auth0 configuration error: Session Secret is required');
+
+  const callbackPath = '/auth/callback';
+  const redirectUri = new URL(callbackPath, options.appBaseUrl);
+
+  const auth0Client = new ServerClient({
+    domain: options.domain,
+    clientId: options.clientId,
+    clientSecret: options.clientSecret,
+    authorizationParams: {
+      redirect_uri: redirectUri.toString(),
+    },
+    transactionStore: new CookieTransactionStore(),
+    stateStore: new StatelessStateStore({
+      secret: options.sessionSecret,
+    }),
+  });
+
+  nitroApp.hooks.hook('request', async (event) => {
+    event.context.auth0Client = auth0Client;
+    event.context.auth0ClientOptions = {
+        appBaseUrl: options.appBaseUrl,
+    };
+  });
+});
