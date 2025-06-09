1e81c0fa155d185b82a408a28a3c505585b0b663
Frederik Prijck <frederik.prijck@okta.com>
2025-06-09 16:03:05 +0200

    chore: add basic example application

---
+++ examples/example-nuxt-web/nuxt.config.ts
@@ -0,0 +1,39 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  modules: ['@auth0/auth0-nuxt'],
+  imports: {
+    autoImport: true,
+  },
+  runtimeConfig: {
+    auth0: {
+      domain: '', // is overridden by NUXT_AUTH0_DOMAIN environment variable
+      clientId: '', // is overridden by NUXT_AUTH0_CLIENT_ID environment variable
+      clientSecret: '', // is overridden by NUXT_AUTH0_CLIENT_SECRET environment variable
+      sessionSecret: '', // is overridden by NUXT_AUTH0_SESSION_SECRET environment variable
+      appBaseUrl: 'http://localhost:3000', // is overridden by NUXT_AUTH0_APP_BASE_URL environment variable
+    },
+  },
+  app: {
+    head: {
+      script: [
+        {
+          src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js',
+          integrity:
+            'sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz',
+          crossorigin: 'anonymous',
+        },
+      ],
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
+          integrity:
+            'sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH',
+          crossorigin: 'anonymous',
+        },
+      ],
+    },
+  },
+});
