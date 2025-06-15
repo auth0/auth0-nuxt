402c696a0ca1f08f198e79dabb3071aea206e8c6
Frederik Prijck <frederik.prijck@okta.com>
2025-06-15 23:35:29 +0200

    chore: update example app to use useAuth0 composable

--- examples/example-nuxt-web/server/middleware/auth.server.ts
+++ examples/example-nuxt-web/server/middleware/auth.server.ts
@@ -2,9 +2,8 @@ export default defineEventHandler(async (event) => {
   const url = getRequestURL(event);
   
   if (url.pathname === '/private') {
-    // TODO: See if there are alternative / better ways to access auth0Client
-    const auth0Client = event.context.auth0Client;
-    const session = await auth0Client.getSession({ event });
+    const auth0Client = useAuth0(event);
+    const session = await auth0Client.getSession();
     if (!session) {
       return sendRedirect(event, '/auth/login?returnTo=' + url.pathname);
     }
