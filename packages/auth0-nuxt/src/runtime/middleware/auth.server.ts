e813187642fb66806eb2f65f86107ce4bf6db92c
Frederik Prijck <frederik.prijck@okta.com>
2025-07-08 13:25:01 +0200

    chore: avoid directly accessing import.meta.client

--- packages/auth0-nuxt/src/runtime/middleware/auth.server.ts
+++ packages/auth0-nuxt/src/runtime/middleware/auth.server.ts
@@ -1,5 +1,6 @@
 import { defineNuxtRouteMiddleware, useNuxtApp } from "#imports";
 import { useSession } from "../composables/use-session";
+import { importMetaServer } from "../helpers/import-meta";
 
 /**
  * Middleware that ensures the useSession composable is populated with the current session.
@@ -7,9 +8,11 @@ import { useSession } from "../composables/use-session";
  * when rendering the page.
  */
 export default defineNuxtRouteMiddleware(async () => {
-  if (import.meta.server) {
+  if (importMetaServer) {
     const app = useNuxtApp();
     const h3Event = app.ssrContext!.event;
+
+    // As we can only import this composable on the server, we need to dynamically import it.
     const { useAuth0 } = await import('../server/composables/use-auth0');
     const auth0Client = useAuth0(h3Event);
 
