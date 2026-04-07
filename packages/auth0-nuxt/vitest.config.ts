0c7fe49ee7f5d7122313c002164994bbe4577c3a
Frederik Prijck <frederik.prijck@okta.com>
2026-04-07 09:09:36 +0200

    fix: workaround for Nuxt adding 'import' to SSR resolve conditions

--- packages/auth0-nuxt/vitest.config.ts
+++ packages/auth0-nuxt/vitest.config.ts
@@ -1,6 +1,22 @@
 import { defineVitestConfig } from '@nuxt/test-utils/config'
 
 export default defineVitestConfig({
+  // Workaround for Nuxt adding 'import' to SSR resolve conditions, which breaks CJS/ESM interop.
+  // See: https://github.com/nuxt/nuxt/pull/34739
+  // See: https://github.com/vitest-dev/vitest/issues/10012#issuecomment-4149566016
+  plugins: [
+    {
+      name: 'patch-conditions',
+      enforce: 'post',
+      configEnvironment(name, config) {
+        if (name === 'ssr') {
+          config.resolve!.conditions = config.resolve!.conditions!.filter(
+            (c: string) => c !== 'import'
+          )
+        }
+      },
+    },
+  ],
   test: {
     environment: 'nuxt',
   },
